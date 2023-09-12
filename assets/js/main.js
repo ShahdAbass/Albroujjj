71db11ea96dd74d5c0ec6638b1603a5c4f12266c
ShahdAbass <shahdabass94@gmail.com>
2023-09-12 23:38:36 +0300

    push4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Medicio
-* Updated: Aug 30 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/medicio-free-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
